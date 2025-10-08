6b0d4dbf190ad2d0c225cca7e9d74e9fc2fcbd97
iqbaleljan09@gmail.com <iqbaleljan09@gmail.com>
2025-10-09 03:20:34 +0800

    fix style deploy 7

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,17 +12,19 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    // UBAH BAGIAN INI
+    protected $proxies = '*'; // Dari null atau [] menjadi '*'
 
     /**
      * The headers that should be used to detect proxies.
      *
      * @var int
      */
+    // DAN UBAH BAGIAN INI
     protected $headers =
         Request::HEADER_X_FORWARDED_FOR |
         Request::HEADER_X_FORWARDED_HOST |
         Request::HEADER_X_FORWARDED_PORT |
-        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_PROTO | // Pastikan baris ini ada
         Request::HEADER_X_FORWARDED_AWS_ELB;
 }
