00bc9ccf4794d73ac717a92e53f3936c866b4eef
iqbaleljan09@gmail.com <iqbaleljan09@gmail.com>
2025-10-09 02:18:55 +0800

    fix style deploy 3

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,17 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
+                'resources/css/app.css', // ini sudah ada
+                'resources/js/app.js',    // ini sudah ada
+                'public/css/admin.css',
+                'public/css/dashboard.css',
+                'public/css/kontak.css',
+                'public/css/landing.css',
+                'public/css/media-pembelajaran.css',
+                'public/css/navbar.css',
+                'public/css/paket.css',
+                'public/css/verify.css',
+                // tambahkan file CSS lainnya di sini
             ],
             refresh: true,
         }),
